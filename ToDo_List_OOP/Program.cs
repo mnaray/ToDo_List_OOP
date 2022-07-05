ec1c0addf18392ba06e1c136715e671e0a7a2456
mnaray <m.naray.inf21@stud.bbbaden.ch>
2022-07-05 23:40:23 +0200

    Add project files.

---
+++ ToDo_List_OOP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ToDo_List_OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the object oriented version of my todo list application.");
+        }
+    }
+}
