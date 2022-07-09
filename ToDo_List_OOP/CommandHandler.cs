e58a71fa2c5d03b4d9f486efd2cf616167f33285
mnaray <m.naray.inf21@stud.bbbaden.ch>
2022-07-09 18:49:25 +0200

    create editing mode in command handler

--- ToDo_List_OOP/CommandHandler.cs
+++ ToDo_List_OOP/CommandHandler.cs
@@ -17,44 +17,74 @@ namespace ToDo_List_OOP
         {
             string input = Console.ReadLine();
 
+            // commands for normal mode
+            if (!Editor.editingModeOn)
+            {
+                switch (input)
+                {
+                    case "help":
+                        Program.HelpMenu();
+                        break;
+
+                    case "exit":
+                        Program.exit = true;
+                        break;
+
+                    case "add todo":
+                        Program.AddTodo();
+                        break;
+
+                    case "description":
+                        Program.PrintDescription();
+                        break;
+
+                    case "rm":
+                        Program.RemoveTodo();
+                        break;
+
+                    case "editor":
+                        Editor.Edit();
+                        break;
+
+                    default:
+                        ErrorHandler.Error(1);
+                        break;
+                }
+            }
+
+            // commands for edit mode only
+            else if (Editor.editingModeOn)
+            {
+                switch (input)
+                {
+                    case "help":
+                        Editor.EditorInstructions();
+                        break;
+
+                    case "exit":
+                        Editor.editingModeOn = false;
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+
+            // commands working in both modes
             switch (input)
             {
-                case "help":
-                    Program.HelpMenu();
-                    break;
-
-                case "exit":
-                    Program.exit = true;
+                case "ls":
+                    Program.ListAll();
                     break;
 
                 case "clear":
                     Console.Clear();
                     break;
 
-                case "add todo":
-                    Program.AddTodo();
-                    break;
-
-                case "ls":
-                    Program.ListAll();
-                    break;
-
-                case "description":
-                    Program.PrintDescription();
-                    break;
-
-                case "rm":
-                    Program.RemoveTodo();
-                    break;
-
-                case "edit":
-                    Editor.Edit();
-                    break;
-
                 default:
-                    ErrorHandler.Error(1);
                     break;
             }
+
         }
     }
 }
--- ToDo_List_OOP/Editor.cs
+++ ToDo_List_OOP/Editor.cs
@@ -9,12 +9,32 @@ namespace ToDo_List_OOP
     class Editor
     {
         // Properties
+        public static bool editingModeOn = false;
 
         // Methods
 
         public static void Edit()
         {
+            editingModeOn = true;
+            EditorInstructions();
 
+            while (editingModeOn)
+            {
+                Console.Write("Enter editor command: ");
+                CommandHandler.Listen();
+            }
+        }
+
+        public static void EditorInstructions()
+        {
+            Console.WriteLine("---------------------------------------------------------------------------------------");
+            Console.WriteLine($"These are all the commands you can use in the editor:\n");
+            Console.WriteLine("exit                     exit the editor");
+            Console.WriteLine("clear                    clear the console, remove all text");
+            Console.WriteLine("edit                     edit a todo");
+            Console.WriteLine("ls                       list all of your active todos, with descending priority");
+            Console.WriteLine("help                     display the editing commands");
+            Console.WriteLine("---------------------------------------------------------------------------------------\n");
         }
     }
 }
