b4cb398eed8852b8ca3dddf20a36c5aa18291db1
mnaray <m.naray.inf21@stud.bbbaden.ch>
2022-07-09 13:04:52 +0200

    edit help menu

--- ToDo_List_OOP/Program.cs
+++ ToDo_List_OOP/Program.cs
@@ -87,11 +87,12 @@ namespace ToDo_List_OOP
             Console.WriteLine("add todo                 add a new todo");
             Console.WriteLine("ls                       list all of your active todos, with descending priority");
             Console.WriteLine("description              display the description of a todo");
-            Console.WriteLine("rm                       remove a todo from the list (without archiving)");
-            Console.WriteLine("edit                     display the editing menu");
-            Console.WriteLine("-----------\nCommands coming soon:");
+            Console.WriteLine("rm                       remove a todo from the list (not archiving)");
+            Console.WriteLine("editor                   display the editing menu");
+            Console.WriteLine("\n-----------\nCommands coming soon:");
             Console.WriteLine("archive                  archive a todo after you're done with it");
             Console.WriteLine("ls archive               list all archived todos");
+            Console.WriteLine("rm archived              remove a todo from the archive");
             Console.WriteLine("save                     save the current state of your todo list locally");
             Console.WriteLine("---------------------------------------------------------------------------------------\n");
         }
