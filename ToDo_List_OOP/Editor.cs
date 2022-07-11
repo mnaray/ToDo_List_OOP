f8f6e8fb293a5b21384a89b310503f14a551c651
mnaray <m.naray.inf21@stud.bbbaden.ch>
2022-07-11 10:52:55 +0200

    add priority and title edit function

--- ToDo_List_OOP/Editor.cs
+++ ToDo_List_OOP/Editor.cs
@@ -36,5 +36,128 @@ namespace ToDo_List_OOP
             Console.WriteLine("help                     display the editing commands");
             Console.WriteLine("---------------------------------------------------------------------------------------\n");
         }
+
+        public static void EditTodo()
+        {
+            while (Program.ConfirmMinLength(Program.todoList))
+            {
+                string input = "";
+                TodoItem selectedTodo = new TodoItem();
+                try
+                {
+                    Console.Write("Enter title of todo you would like to edit: ");
+                    input = Console.ReadLine();
+
+                    // todo: optimize this section!
+                    // don't make it loop twice, looking
+                    // for the same thing in the same list
+
+                    if (!Program.CheckTitleExistance(Program.todoList, input))
+                    {
+                        throw new Exception();
+                    }
+
+                    foreach (TodoItem item in Program.todoList.todoItemsList)
+                    {
+                        if (item.title == input)
+                        {
+                            selectedTodo = item;
+                        }
+                    }
+
+                    EditTitle(selectedTodo);
+                    EditPriority(selectedTodo);
+                    EditDescription(selectedTodo);
+
+                    break;
+                }
+                catch (Exception)
+                {
+                    if (input == "")
+                    {
+                        ErrorHandler.Error(2);
+                    }
+                    else
+                    {
+                        ErrorHandler.Error(3);
+                    }
+                }
+
+
+            }
+        }
+        static void EditTitle(TodoItem selectedTodo)
+        {
+            while (true)
+            {
+                string input = "";
+                try
+                {
+                    Console.Write($"\nCurrent title:  {selectedTodo.title}\n");
+                    Console.Write($"New title:      ");
+                    input = Console.ReadLine();
+
+                    if (input == "")
+                    {
+                        throw new Exception();
+                    }
+
+                    selectedTodo.title = input;
+
+                    Console.WriteLine("Successfully changed title.\n");
+
+                    break;
+                }
+                catch (Exception)
+                {
+                    if (input == "")
+                    {
+                        ErrorHandler.Error(2);
+                    }
+                    else
+                    {
+                        ErrorHandler.Error(6);
+                    }
+                }
+            }
+        }
+
+        static void EditPriority(TodoItem selectedTodo)
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine($"\nCurrent priority:  {selectedTodo.priority}\n");
+
+                    int i = 0;
+                    foreach (var item in Enum.GetValues(typeof(TodoItem.priorityEnum)))
+                    {
+                        Console.WriteLine(i + ") " + item.ToString());
+                        i++;
+                    }
+
+                    Console.Write("\nEnter the number of the  new priority here: ");
+                    selectedTodo.priority = Convert.ToInt32(Console.ReadLine());
+                    if (!Enum.IsDefined(typeof(TodoItem.priorityEnum), selectedTodo.priority))
+                    {
+                        throw new Exception();
+                    }
+
+                    Console.WriteLine("Successfully changed priority.\n");
+
+                    break;
+                }
+                catch (Exception)
+                {
+                    ErrorHandler.Error(3);
+                }
+            }
+        }
+
+        static void EditDescription(TodoItem selectedTodo)
+        {
+            
+        }
     }
 }
